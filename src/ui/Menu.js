26613e3c0ce428420fd1d6b2ca97e6e182e4913e
MitcovaKate <katya.mitkova@bk.ru>
2024-06-10 23:20:36 +0300

    message

--- src/ui/Menu.js
+++ src/ui/Menu.js
@@ -1,27 +1,31 @@
 const Menu = ({items,sortAsc}) => {
-  
+  let [message, setMessage]=useState(undefined)
     return (
         <>
             <h1>Menu</h1>
+            {message ? <p style={{border:'1px solid green'}}>{message}</p>:<></>}
+        
             <ul>
                 {items
                     .sort(
                         (item1, item2) => (sortAsc ? 1: -1) * (item1.price.amount - item2.price.amount)
                     )
                     .map(item => 
-                        <li>
+                        <li key={idx}>
                             <h2>{item.name}</h2>
                             <img src={item.image} width="200"/>                    
                             <p>{item.price.amount}{item.price.currency}</p>
-                       <button
-                       onClick={e=>{
+            <button
+                       onClick={
+                        async e=>{
                         let itemId=e.target.dataset.productId
-                        orderItem(itemId)
+                      let data = await  orderItem(itemId)
+                   setMessage(data.message)
                        }} // wrapper
                        data-product-id={item.id}
                        >
                       ORDER
-                       </button>
+            </button>
                        
                         </li>
                 )}
